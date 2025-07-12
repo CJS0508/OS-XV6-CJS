8332207a17460106859c38fc458ce498d64d24e2
CJS0508 <2353127@tongji.edu.cn>
2025-07-12 07:21:06 -0700

    Finished Lab 4: traps

--- kernel/param.h
+++ kernel/param.h
@@ -1,3 +1,4 @@
+
 #define NPROC        64  // maximum number of processes
 #define NCPU          8  // maximum number of CPUs
 #define NOFILE       16  // open files per process
@@ -9,5 +10,5 @@
 #define MAXOPBLOCKS  10  // max # of blocks any FS op writes
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
-#define FSSIZE       1000  // size of file system in blocks
+#define FSSIZE       10000  // size of file system in blocks
 #define MAXPATH      128   // maximum file path name
